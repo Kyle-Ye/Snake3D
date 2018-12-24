6c2d6331a45dcdba5980192c1f43b6a4d6fae461
Y7000 <kyle201817147@gmail.com>
2018-12-24 22:36:48 +0800

    bug(show nothing)

--- Head/Game.h
+++ Head/Game.h
@@ -1,23 +1,39 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-enum GameState
-{
+#include <vector>
+
+#include "GameLevel.h"
+enum GameState {
 	GAME_ACTIVE,
 	GAME_MENU,
 	GAME_WIN
 };
+
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100, 20);
+// Initial velocity of the player paddle
+const GLfloat PLAYER_VELOCITY(500.0f);
+
+// Game holds all game-related state and functionality.
+// Combines all game-related data into a single class for
+// easy access to each of the components and manageability.
 class Game
 {
 public:
-	GameState State;
-	GLboolean Keys[1024];
-	GLuint Width, Height;
+	// Game state
+	GameState              State;
+	GLboolean              Keys[1024];
+	GLuint                 Width, Height;
+	std::vector<GameLevel> Levels;
+	GLuint                 Level;
+	// Constructor/Destructor
 	Game(GLuint width, GLuint height);
 	~Game();
+	// Initialize game state (load all shaders/textures/levels)
 	void Init();
+	// GameLoop
 	void ProcessInput(GLfloat dt);
 	void Update(GLfloat dt);
 	void Render();
 };
-
--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,9 +1,12 @@
 #include "../Head/Game.h"
 #include "../Head/ResourceManager.h"
 #include "../Head/SpriteRenderer.h"
+#include "../Head/GameObject.h"
 
 // Game-related State data
 SpriteRenderer  *Renderer;
+GameObject      *Player;
+
 
 Game::Game(GLuint width, GLuint height)
 	: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -14,39 +17,74 @@ Game::Game(GLuint width, GLuint height)
 Game::~Game()
 {
 	delete Renderer;
+	delete Player;
 }
 
 void Game::Init()
 {
-	ResourceManager::LoadShader("shader/sprite.vs", "shader/sprite.fs", nullptr, "sprite");
+	// Load shaders
+	ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
 	// Configure shaders
-	glm::mat4 projection = glm::mat4(1.0f);
-	projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
-	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width), static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("sprite", 0);
 	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
 	// Load textures
-	ResourceManager::LoadTexture("Resource/awesomeface.png", GL_TRUE, "face");
+	ResourceManager::LoadTexture("textures/background.jpg", GL_FALSE, "background");
+	ResourceManager::LoadTexture("textures/awesomeface.png", GL_TRUE, "face");
+	ResourceManager::LoadTexture("textures/block.png", GL_FALSE, "block");
+	ResourceManager::LoadTexture("textures/block_solid.png", GL_FALSE, "block_solid");
+	ResourceManager::LoadTexture("textures/paddle.png", true, "paddle");
 	// Set render-specific controls
 	Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+	// Load levels
+	GameLevel one; one.Load("levels/one.lvl", this->Width, this->Height * 0.5);
+	GameLevel two; two.Load("levels/two.lvl", this->Width, this->Height * 0.5);
+	GameLevel three; three.Load("levels/three.lvl", this->Width, this->Height * 0.5);
+	GameLevel four; four.Load("levels/four.lvl", this->Width, this->Height * 0.5);
+	this->Levels.push_back(one);
+	this->Levels.push_back(two);
+	this->Levels.push_back(three);
+	this->Levels.push_back(four);
+	this->Level = 0;
+	// Configure game objects
+	glm::vec2 playerPos = glm::vec2(this->Width / 2 - PLAYER_SIZE.x / 2, this->Height - PLAYER_SIZE.y);
+	Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(GLfloat dt)
 {
-	/*glm::mat4 projection = glm::mat4(1.0f);
-	projection = glm::perspective(glm::radians(ResourceManager::camera.Zoom), (float)this->Width / (float)this->Height, 0.1f, 100.0f);
-	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
 
-	glm::mat4 view = ResourceManager::camera.GetViewMatrix();
-	ResourceManager::GetShader("sprite").SetMatrix4("view", view);*/
 }
 
 
 void Game::ProcessInput(GLfloat dt)
 {
-
+	if (this->State == GAME_ACTIVE)
+	{
+		GLfloat velocity = PLAYER_VELOCITY * dt;
+		// Move playerboard
+		if (this->Keys[GLFW_KEY_A])
+		{
+			if (Player->Position.x >= 0)
+				Player->Position.x -= velocity;
+		}
+		if (this->Keys[GLFW_KEY_D])
+		{
+			if (Player->Position.x <= this->Width - Player->Size.x)
+				Player->Position.x += velocity;
+		}
+	}
 }
 
 void Game::Render()
 {
-	Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+	if (this->State == GAME_ACTIVE)
+	{
+		// Draw background
+		Renderer->DrawSprite(ResourceManager::GetTexture("background"), glm::vec2(0, 0), glm::vec2(this->Width, this->Height), 0.0f);
+		// Draw level
+		this->Levels[this->Level].Draw(*Renderer);
+		// Draw player
+		Player->Draw(*Renderer);
+	}
 }
